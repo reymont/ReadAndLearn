83705239f4f1ea6766538f6191c2dfddc9af7f4d
liyang <3195049377@qq.com>
2017-11-27 11:11:13 +0800

    rabbitmq

--- back-end/java/mq/rabbitmq/RabbitMQ实战/ch4/alert_producer.py
+++ back-end/java/mq/rabbitmq/RabbitMQ实战/ch4/alert_producer.py
@@ -12,6 +12,7 @@ opt_parser.add_option("-m",
                       dest="message",
                       help="Message text for alert.")
 
+# 从命令行获取消息和路由键
 args = opt_parser.parse_args()[0]
 creds_broker = pika.PlainCredentials("alert_user", "alertme")
 conn_params = pika.ConnectionParameters("localhost",
@@ -20,3 +21,16 @@ conn_params = pika.ConnectionParameters("localhost",
 conn_broker = pika.BlocklingConnection(conn_params)
 channel = conn_broker.channel()
 
+# 创建告警信息并标记合适的路由键
+msg = json.dumps(args.message)
+msg_props = pika.BasicProperties()
+msg_props.content_type = "application/json"
+msg_props.durable = False
+channel.basic_publish(body=msg,
+                      exchange="alerts",
+                      properties=msg_props,
+                      routing_key=args.routing_key)
+
+print ("Sent message %s tagged with routing key '%s' to " + \
+       "exchange '/'.") % (json.dumps(args.message),
+                          args.routing_key)
