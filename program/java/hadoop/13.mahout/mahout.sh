22005f4ad6b6ea1c70b3a06c97f77e949ff78e6a
reymont <258227346@qq.com>
2018-08-23 09:35:49 +0800

    harbo

--- program/java/hadoop/13.mahout/mahout.sh
+++ program/java/hadoop/13.mahout/mahout.sh
@@ -9,3 +9,19 @@ Pig0.12.1
 Ubuntu12
 Jdk1.7
 
+http://172.20.62.34:50070/dfshealth.jsp
+http://172.20.62.34:50075/browseDirectory.jsp?namenodeInfoPort=50070&dir=/
+
+### https://blog.csdn.net/sofuzi/article/details/80439400
+### 1. 上传文件
+# sport文件夹：
+    # 用于训练文本分类器
+    # 包含了多个子文件夹，每个子文件夹都是一个分类的文章
+    # 在现实项目中，该原始数据需要人工收集
+# user-sport：
+    # 待分类的文本
+hadoop dfs -put sport /dataguru/week8
+hadoop dfs -put user-sport/ /dataguru/week8
+
+
+hadoop jar MRTokenize.jar tokenize.TokenizeDriver /dataguru/week8/sport /dataguru/week8/fenciout
