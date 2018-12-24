3b0c53fcec6e8c2df48399c8f5993017989f458e
reymont <258227346@qq.com>
2018-12-24 21:03:01 +0800

    docker

--- aliyun/3.业务实时监控服务ARMS/ARMS.md
+++ aliyun/3.业务实时监控服务ARMS/ARMS.md
@@ -1,3 +1,9 @@
 
 
+## 1. 文档
 1. https://help.aliyun.com/product/34364.html
+
+## 2. 价格
+1. https://www.aliyun.com/price/product#/arms/detail
+2. https://arms.console.aliyun.com/price/
+
---
+++ program/docker/2.内核/1.namespace/skeleton.c
@@ -0,0 +1,51 @@
+// http://crosbymichael.com/creating-containers-part-1.html
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define STACKSIZE (1024*1024)
+static char child_stack[STACKSIZE];
+
+struct clone_args {
+        char **argv;
+};
+
+// child_exec is the func that will be executed as the result of clone
+static int child_exec(void *stuff)
+{
+        struct clone_args *args = (struct clone_args *)stuff;
+        if (execvp(args->argv[0], args->argv) != 0) {
+                fprintf(stderr, "failed to execvp argments %s\n",
+                        strerror(errno));
+                exit(-1);
+        }
+        // we should never reach here!
+        exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv)
+{
+        struct clone_args args;
+        args.argv = &argv[1];
+
+        int clone_flags = SIGCHLD;
+
+        // the result of this call is that our child_exec will be run in another
+        // process returning it's pid
+        pid_t pid =
+            clone(child_exec, child_stack + STACKSIZE, clone_flags, &args);
+        if (pid < 0) {
+                fprintf(stderr, "clone failed WTF!!!! %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+        }
+        // lets wait on our child process here before we, the parent, exits
+        if (waitpid(pid, NULL, 0) == -1) {
+                fprintf(stderr, "failed to wait pid %d\n", pid);
+                exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+}
